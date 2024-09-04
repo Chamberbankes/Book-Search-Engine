6899d0cacff92bec5e10da843e46696fb5b52ba0
unknown <chamberbankes@gmail.com>
2024-09-04 17:45:04 -0400

    readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Book-Search-Engine
+# Book-Search-Engine [![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+This project will allow the user to search for books by title.
+
+
+## Usage
+
+To run the project locally you must first enter "npm install" to install the dependencies and then enter "npm run start" .
+
+![Image](./assets/Screenshot%202024-09-04%20173355.png)
+
+Project repo: https://github.com/Chamberbankes/Book-Search-Engine
+
+## Credits
+
+The MERN student mini project.
+
+## License
+
+MIT [![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+
+
+## Questions
+You can find more examples of my work at https://github.com/Chamberbankes?tab=repositories.
--- server/server.js
+++ server/server.js
@@ -16,21 +16,21 @@ const server = new ApolloServer({
 const startApolloServer = async () => {
   await server.start();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use('/graphql', expressMiddleware(server));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use('/graphql', expressMiddleware(server));
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
 
-app.use(routes);
+  app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-});
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
 };
 
 startApolloServer();
